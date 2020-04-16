b3d735b6ce7e0b62a9442cd44752dba9870200e0
Dominic Gaiero <dpgaiero@gmail.com>
2020-04-15 20:42:05 -0700

    feat: add region props for 0/2

--- main.m
+++ main.m
@@ -4,20 +4,24 @@ clc
 close all
 %%
 Z = getNumbers('mnist_train0.jpg');
-areas = arrayfun(@(a) getRegionProps(a.nums,'Area'),Z);
+Area = arrayfun(@(a) getRegionProps(a.nums,'Area'),Z);
 perimeters = arrayfun(@(a) getRegionProps(a.nums,'Perimeter'),Z);
-Circularity = (4*pi*areas)./(perimeters.^2);
+Eccentricity = arrayfun(@(a) getRegionProps(a.nums,'Eccentricity'),Z);
+Orientation = arrayfun(@(a) getRegionProps(a.nums,'Orientation'),Z);
+Circularity = (4*pi*Area)./(perimeters.^2);
 MinorAxis = arrayfun(@(a) getRegionProps(a.nums,'MinorAxisLength'),Z);
 %%
 O = getNumbers('mnist_train1.jpg');
 OnesAreas = arrayfun(@(a) getRegionProps(a.nums,'Area'),O);
 OnesPerimeters = arrayfun(@(a) getRegionProps(a.nums,'Perimeter'),O);
+OnesEccentricity = arrayfun(@(a) getRegionProps(a.nums,'Eccentricity'),O);
 OnesCircularity = (4*pi*OnesAreas)./(OnesPerimeters.^2);
 OnesMinorAxis = arrayfun(@(a) getRegionProps(a.nums,'MinorAxisLength'),O);
 %%
 T = getNumbers('mnist_train2.jpg');
 TwosAreas = arrayfun(@(a) getRegionProps(a.nums,'Area'),T);
 TwosPerimeters = arrayfun(@(a) getRegionProps(a.nums,'Perimeter'),T);
+TwosOrientation = arrayfun(@(a) getRegionProps(a.nums,'Orientation'),T);
 TwosCircularity = (4*pi*TwosAreas)./(TwosPerimeters.^2);
 TwosMinorAxis = arrayfun(@(a) getRegionProps(a.nums,'MinorAxisLength'),T);
 %%
@@ -29,7 +33,7 @@ hold off
 legend('zeros','ones');
 %%
 figure(11)
-histogram(areas)
+histogram(Area)
 hold on
 histogram(OnesAreas)
 hold off
@@ -49,10 +53,19 @@ scatter(OnesPerimeters,OnesCircularity,'.');
 hold off;
 %%
 figure(14)
-scatter(areas,MinorAxis,'.');
+scatter(Area,MinorAxis,'.');
 hold on;
 scatter(OnesAreas,OnesMinorAxis,'.');
 hold off;
 legend('zeros','ones');
+xlabel('Area')
+ylabel('MinorAxisLength');
+%%
+figure(14)
+scatter(Orientation,Circularity,'.');
+hold on;
+scatter(TwosOrientation,TwosCircularity,'.');
+hold off;
+legend('zeros','twos');
 xlabel('Area')
 ylabel('MinorAxisLength');
